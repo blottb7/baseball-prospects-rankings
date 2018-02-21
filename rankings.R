dcb17b759c508277602e3b2d3113d4bab1480587
Benjamin Blott <benjaminblott@verticalonellc.com>
2018-02-21 12:01:48 -0600

    Update rankings.R

--- rankings.R
+++ rankings.R
@@ -3,12 +3,11 @@ library(rvest)
 library(dplyr)
 library(tidyr)
 library(stringr)
-#library(matrixStats)  #for rowRanks fn()
 
 #Description:
 #Read in ranking lists from prominent baseball prospects lists:
 #fangraphs, baseball america, baseball prospectus
-#also add mlb pipeline, but weight it less
+#also add mlb pipeline, but weigh it less
 #second tier: mlb, razzball, katoh
 
 #baseball america
@@ -29,10 +28,8 @@ bbp <- tibble(bbp)
 
 #fangraphs
 url <- read_html('https://www.fangraphs.com/blogs/2018-top-100-prospects/')
-webpage <- html_nodes(url, '.align-left')
-webpage <- html_text(webpage)
-webpage <- tibble(webpage)
 
+#reads in 100 x 7 table
 fg <- html_nodes(url, '.table-wrapper td')
 fg <- html_text(fg)
 fg <- data.frame(matrix(fg, nrow = 100, ncol = 7, byrow = TRUE))
@@ -45,11 +42,6 @@ names(fg) <- c("fg_rank", "name", "team_abb", "age", "fg_pos", "fv", "variance")
 #1.bba
 
 #separate into rank, name, pos, team
-#copy <- bba
-#split_copy <- str_split(copy, fixed("<"), n = 2, simplify = TRUE)
-#split_copy <- unlist(t(str_split(copy, "<", simplify = TRUE)))
-# split_copy <- t(str_split(copy, "<", simplify = TRUE))
-
 bba1 <- bba %>%
   separate(bba, c("name", "bba_pos", "team"), "\\|")
 
@@ -70,10 +62,11 @@ bba1$team <- ifelse(bba1$team == "D'backs", "Diamondbacks", bba1$team)
 bba1$team <- ifelse(bba1$team == "Red", "Red Sox", bba1$team)
 bba1$team <- ifelse(bba1$team == "White", "White Sox", bba1$team)
 
-#remove periods from names
+#remove periods from names for consistent joining
 bba1$name <- str_replace_all(bba1$name, "\\.", "")
 
 #2. bbp
+
 bbp1 <- bbp
 #clean the one comma'd name
 bbp1$bbp <- ifelse(bbp1$bbp == "Fernando Tatis, Jr., SS, San Diego Padres",
@@ -99,7 +92,8 @@ for(i in 1:nrow(bbp1)) {
 #homogenize names
 bbp1$name <- ifelse(bbp1$name == "Mitch White", "Mitchell White", bbp1$name)
 
-#clean fangraphs
+#fangraphs
+
 fg1 <- fg
 
 #clean periods and commas from player names
@@ -120,6 +114,7 @@ df <- bba1 %>%
 df$bba_rank <- as.numeric(df$bba_rank)
 df$fg_rank <- as.numeric(as.character(df$fg_rank))
 
+#create a single team column
 df$team <- ifelse(!is.na(df$team), df$team,
                   ifelse(!is.na(df$city_team), df$city_team,
                          as.character(df$team_abb)))
@@ -127,6 +122,7 @@ df$team <- ifelse(!is.na(df$team), df$team,
 df <- df %>%
   select(-city_team, -team_abb)
 
+#create a single player position column
 df$position <- ifelse(!is.na(df$bba_pos), df$bba_pos,
                       ifelse(!is.na(df$bbp_pos), df$bbp_pos,
                              as.character(df$fg_pos)))
@@ -134,10 +130,13 @@ df$position <- ifelse(!is.na(df$bba_pos), df$bba_pos,
 df <- df %>%
   select(-bba_pos, -bbp_pos, -fg_pos)
 
+#create a single ranking var
+#subset ranks
 df_ranks <- df %>%
   select(bba_rank, bbp_rank, fg_rank)
 
-df_ranks$rank <- 0
+#take the rowMeans of rankings; take single ranking if player ranked only once
+df_ranks$rank <- 0  #initialize rank var
 for(i in 1:nrow(df_ranks)) {
   
   df_ranks$rank[i] <- ifelse(sum(is.na(df_ranks[i,])) <= 1, rowMeans(df_ranks[i,1:3], na.rm = TRUE),
@@ -147,21 +146,28 @@ for(i in 1:nrow(df_ranks)) {
   
 }
 
+#select only rank for binding with players
 df_ranks_new <- df_ranks %>%
   select(rank)
 
+#bind the new rank
 df_new <- df %>%
   bind_cols(df_ranks_new)
 
-df_new$rank <- round(as.numeric(df_new$rank))
+df_new$rank <- round(as.numeric(df_new$rank))  #remove decimal players
 
 df_new <- df_new %>%
-  arrange(rank, bba_rank)
+  arrange(rank, bba_rank, bbp_rank)  #arrange by rank, with bba and bbp ranks as tie-breakers
 
+#organize columns nicely
 df_new1 <- df_new %>%
   select(mean_rank = rank, name, position, age, team, fv, variance, bba_rank, bbp_rank, fg_rank)
 
+#TO DO
 #organize by position
+
+#FIX BBP VLAD GUERR! ITS COMING UP AS UNIQUE TO THE OTHER SPELLING
+#also change Jordon to Jo Adell in bbp
 
 write.csv(df_new1, "C:/Users/Ben/Desktop/R Projects/baseball_prospects_2018.csv")
 # df <- df %>%
